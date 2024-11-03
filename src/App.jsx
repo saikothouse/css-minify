6dcd8533372d0178ab8329b90d003a46c3ef8698
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-03 21:42:06 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import CssMinifier from './CssMinifier';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Features from './components/Features';
+import CSSMinifier from './components/CSSMinifier';
 
 const App = () => {
   return (
     <div>
-      <CssMinifier />
+      <Header />
+      <About />
+      <Features />
+      <CSSMinifier />
+      <Footer />
     </div>
   );
 };
